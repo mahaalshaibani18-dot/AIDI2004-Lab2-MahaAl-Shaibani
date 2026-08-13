08ec0d4dff02dda0a784b11013ca784e4e78958c
Maha Al-Shaibani <maha.alshaibani.18@gmail.com>
2026-08-13 16:10:45 -0400

    Update greeting message - Maha AlShaibani

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py - Created by Maha Al-Shaibani
 def greet (name):
-    return f"Hello, {name}! Welcome to AIDI 2004"
+    return f"Hello, {name}! How are you today?"
 
 if __name__ == "__main__":
     print(greet("Maha"))
